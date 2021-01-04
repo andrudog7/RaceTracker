708129d9eaeafeb379a861f8cf11eb872ac4f944
--add <andrewksmoker@gmail.com>
2021-01-04 16:45:49 -0500

    Create Race Table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+    validate :email, presence :true, uniqueness :true
+    validate :first_name, presence :true
+    validate :last_name, presence :true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_212823) do
+ActiveRecord::Schema.define(version: 2021_01_04_213511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "races", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
