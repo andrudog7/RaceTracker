781498ae47788d337a6fc29daa8c33180a38d52d
--add <andrewksmoker@gmail.com>
2021-01-05 13:18:01 -0500

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
     has_many :races, through: :statistics
     has_many :likes
     validates :email, presence: true, uniqueness: true
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :password, confirmation: true 
+    validates :first_name, :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/,
+        message: "only allows letters" }
+    # validates :last_name, presence: true
+    validates :password, confirmation: true
+    validates :password_confirmation, presence: true
 end
