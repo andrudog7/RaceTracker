f3cd26eb1050156aeb6d516b7fa23316369a97b7
--add <andrewksmoker@gmail.com>
2021-01-05 16:33:41 -0500

    menu bar styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,4 @@
 class ApplicationController < ActionController::Base
     def home
     end
-
-    helpers do 
-        def current_user
-            @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-        end
-
-        def logged_in?
-            !!current_user 
-        end
-
-    end
 end
