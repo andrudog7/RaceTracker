34055c56145eca24c5786a5750e2fdfcc0ffa5fd
--add <andrewksmoker@gmail.com>
2021-01-08 17:46:33 -0500

    create new race with type

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,6 +1,32 @@
 class RacesController < ApplicationController
 
     def new 
-        @race = Race.new 
+        @race = Race.new
+        @race.build_type
+    end
+
+    def create
+        if logged_in?
+            @race = Race.find_or_create_by(:name => race_params[:name], :location => race_params[:location], :date => race_params[:date], :public => race_params[:public])
+            @race.owner = current_user
+            @race.type = Type.find_by(:id => race_params[:type_attributes][:id])
+            if @race.save
+                redirect_to race_path(@race)
+            else
+                redirect_to new_race_path
+            end
+        else
+            redirect_to '/'
+        end
+    end
+
+    def show 
+        @race = Race.find(params[:id])
+    end
+
+    private 
+
+    def race_params
+        params.require(:race).permit(:name, :location, :date, :public, type_attributes: [:id, :name])
     end
 end
--- app/models/race.rb
+++ app/models/race.rb
@@ -3,6 +3,11 @@ class Race < ApplicationRecord
     has_many :users, through: :statistics
     belongs_to :type
     belongs_to :owner, :class_name => "User"
+    accepts_nested_attributes_for :type
 
-    
+    def type_attributes=(attributes)
+        byebug
+    end
+
+
 end
